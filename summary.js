d24795ac078ffc9c0314e7952eeb7eb45a005bfa
Mohaiminul-Islam-Bhuiyan <rafe.aust@gmail.com>
2022-09-12 08:47:51 +0600

    final

---
+++ summary.js
@@ -0,0 +1,8 @@
+/**
+ * 1.1: variable (let, const) three types of variable
+ * 1.2: condition (<, >, ===, !==, <==, >==) && ||, if-else if -else
+ * 
+ * 
+ * 
+ * ------------see text document on very first on the module-------------
+ */
