aeff13ad63db447c85b7c874e041edc21b038857
Mohaiminul-Islam-Bhuiyan <rafe.aust@gmail.com>
2022-09-12 08:53:26 +0600

    complete

--- summary.js
+++ summary.js
@@ -6,3 +6,27 @@
  * 
  * ------------see text document on very first on the module-------------
  */
+
+
+// object declaration shorthand
+
+const x = 50;
+const y = 100;
+const obj = {
+    x:x, 
+    y:y
+}
+
+// shortcut
+const obj1 = {x, y}
+
+const name = 'salimullah'
+const area = 'dhaka'
+
+const nobab = {
+    name : name,
+    area : area
+}
+
+//shortcut
+const nobab2 = {name, area}
